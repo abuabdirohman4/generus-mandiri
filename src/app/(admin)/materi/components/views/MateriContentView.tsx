41f689aa2bd18bffb00bda8b47dcf03527794265
Abu Abdirohman <abuabdirohman4@gmail.com>
2025-12-09 06:46:35 +0700

    feat: Temporarily disable 'Create New Meeting' button in MateriContentView.

--- src/app/(admin)/materi/components/views/MateriContentView.tsx
+++ src/app/(admin)/materi/components/views/MateriContentView.tsx
@@ -210,7 +210,7 @@ export default function MateriContentView({
                             <span className="md:inline">Tambah Materi</span>
                         </button> */}
 
-                        <button
+                        {/* <button
                             onClick={onCreateItem}
                             className=""
                             title="Buat Pertemuan Baru"
@@ -218,7 +218,7 @@ export default function MateriContentView({
                             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                             </svg>
-                        </button>
+                        </button> */}
                     </>
                 )}
             </div>
